ad294ef3fe2393441ccded23715f4be46ea811af
Victor Chung <chungcoelhove070@gmail.com>
2026-05-22 12:34:37 -0500

    acabamos instancias todo nuestro en el repositorio de github

---
+++ src/test/java/org/syncastra/ejemplospring/EjemploSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.syncastra.ejemplospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
